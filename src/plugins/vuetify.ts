d34ae638f0e42a322168f6b8d2c4da5cbfdb1def
Kira <hanzomein228@gmail.com>
2025-11-12 18:26:51 +0300

    настроил сабмодуль

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -12,13 +12,9 @@ export default new Vuetify({
     options: {
       customProperties: true
     },
-
   },
   typography: {
     fontFamily: '"Roboto", "Inter", "Segoe UI", Arial, sans-serif',
   },
   icons: { iconfont: 'mdi' },
-
-
-
 });
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const path = require("path");
+
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@shared": path.resolve(__dirname, "src/shared-ui/src"),
+      },
+    },
+  },
 };
