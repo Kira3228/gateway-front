ea278f9333bcd095d53fff1622db14593b7f6fb2
Kira <hanzomein228@gmail.com>
2025-11-23 01:05:38 +0300

    добавил сорт панель на статусы

--- src/widgets/StatusHistory/StatusHistory.vue
+++ src/widgets/StatusHistory/StatusHistory.vue
@@ -1,6 +1,6 @@
 <template>
   <ext-data-card title="История изменения">
-    <!-- <status-history-sort-panel /> -->
+    <status-history-sort-panel />
     <status-history-virtual-scroll
       :error="error"
       :is-loading="isLoading"
@@ -16,6 +16,7 @@ import { toRef, watch } from "vue";
 import { useStatusHistoryOrderSwitchModel } from "@/features/statusHistoryOrderSwitch/model/model";
 import { useStatusHistoryStore } from "@/entities/statusHistory/model/store";
 import { storeToRefs } from "pinia";
+import StatusHistorySortPanel from "./StatusHistorySortPanel/StatusHistorySortPanel.vue";
 
 interface IProps {
   id: string;
