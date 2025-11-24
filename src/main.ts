cc4dc1c9e6955832a8ccdd615d5c1746043b0c4f
Kira <hanzomein228@gmail.com>
2025-11-24 03:06:16 +0300

    убрал Vuex

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import '@/assets/main.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
 import vuetify from './plugins/vuetify'
 import { createPinia, PiniaVuePlugin } from "pinia"
 
@@ -11,7 +11,6 @@ Vue.use(PiniaVuePlugin)
 const pinia = createPinia()
 new Vue({
   pinia,
-  store,
   vuetify,
   router,
   render: h => h(App)
