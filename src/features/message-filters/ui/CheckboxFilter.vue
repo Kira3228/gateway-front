0ee9054f6f37b12c282c25cf1373b160bfff728c
Kira <hanzomein228@gmail.com>
2026-02-24 14:18:50 +0300

    Добавил кнопки очистки в фильтры

--- src/features/message-filters/ui/CheckboxFilter.vue
+++ src/features/message-filters/ui/CheckboxFilter.vue
@@ -1,10 +1,12 @@
 <template>
   <AccordionSection :title="title">
     <CheckBoxGroup :items="items" :value="value" @input="handleInput" />
+    <Button @click="handleClick" small text>Сбросить</Button>
   </AccordionSection>
 </template>
 <script setup lang="ts" generic="T">
 import { AccordionSection } from "@/shared-ui/src/components/AccordionSection";
+import { Button } from "@/shared-ui/src/components/Button";
 import { CheckBoxGroup } from "@/shared-ui/src/components/CheckBoxGroup";
 
 interface Props {
@@ -18,5 +20,8 @@ const emit = defineEmits<{ (e: `input`, data: string[]): void }>();
 
 const handleInput = (data: string[]) => {
   emit(`input`, data);
+};
+const handleClick = () => {
+  emit(`input`, []);
 };
 </script>
