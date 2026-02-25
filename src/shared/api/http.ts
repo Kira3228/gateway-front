d757ae12f5de477a7dde84cf24b2e2fdcde8c316
Kira <hanzomein228@gmail.com>
2026-02-25 10:44:07 +0300

    пофиксил http

--- src/shared/api/http.ts
+++ src/shared/api/http.ts
@@ -6,19 +6,20 @@ import qs from 'qs';
 const api = axios.create({
   baseURL: BASE_URL,
   paramsSerializer: (params) => {
+    console.log(`params`, params);
+
     const searchParams = new URLSearchParams();
 
     Object.entries(params).forEach(([key, value]) => {
       if (Array.isArray(value)) {
-        value.forEach(item => {
-          if (item !== undefined && item !== null && item !== '') {
-            searchParams.append(key, item);
-          }
-        });
+        if (value.length > 0) {
+          searchParams.append(key, value.join(','));
+        }
       } else if (value !== undefined && value !== null && value !== '') {
         searchParams.append(key, value as string);
       }
     });
+    console.log(`searchParams`, searchParams.toString());
 
     return searchParams.toString();
   }
